81f3c753656f991085502a173b184a43d9350bb3
Brijesh Shiroya <brijesh.s@simformsolutions.com>
2019-09-16 14:51:28 +0530

    feat: add functions into index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import { NativeModules } from 'react-native';
-
 const { ReactNativeEngage } = NativeModules;
 
-export default ReactNativeEngage;
+function registerUser({ apiKey, birthDate, gender = null, feature1 = null, feature2 = null, feature3 = null, callback } = {}) {
+  ReactNativeEngage.registerUser(apiKey, birthDate, gender, feature1, feature2, feature3, callback);
+};
+
+export default {
+  initialize: ReactNativeEngage.initialize,
+  isInitialized: ReactNativeEngage.isInitialized,
+  isScanOnGoing: ReactNativeEngage.isScanOnGoing,
+  startScan: ReactNativeEngage.startScan,
+  stopScan: ReactNativeEngage.stopScan,
+  updateApiKey: ReactNativeEngage.updateApiKey,
+  logout: ReactNativeEngage.logout,
+  setRegionParams: ReactNativeEngage.setRegionParams,
+  config: ReactNativeEngage.config,
+  registerUser: registerUser
+};
